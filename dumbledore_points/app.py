ee4e647349fcd0559a5e737d7764971509f35d9b
Diana Patron <patrondiana13@hotmail.com>
2020-08-26 18:07:56 -0500

    House leaderboard and formatting of points

--- dumbledore_points/app.py
+++ dumbledore_points/app.py
@@ -11,7 +11,8 @@ dynamo = boto3.resource('dynamodb')
 HOGWARTS_ALUMNI_TABLE = dynamo.Table('Hogwarts_Alumni')
 
 HEADMASTER = ['dianapatrong']
-HOGWARTS_HOUSES = ['GRYFFINDOR', 'SLYTHERIN', 'RAVENCLAW', 'HUFFLEPUFF']
+HOGWARTS_HOUSES = ['gryffindor', 'slytherin', 'ravenclaw', 'hufflepuff']
+PREFIXES = ["In the lead is ","Second place is ","Third place is ","Fourth place is "]
 
 
 def verify_request(event):
@@ -62,16 +63,18 @@ def get_house_points(house):
 
 
 def parse_potential_house(house):
+    print("hosue", house)
     target_house = house.lower()
     if target_house in HOGWARTS_HOUSES:
         return target_house
     else:
-        return "THAT IS NOT A HOUSE "
+        return None
 
 
 def create_wizard(wizard, house):
     wizard_found = check_user_permission(wizard)
     wizard = remove_at(wizard)
+    print("WIZARD FOUND: ", wizard_found)
     if not wizard_found:
         HOGWARTS_ALUMNI_TABLE.put_item(
             Item={
@@ -88,11 +91,12 @@ def create_wizard(wizard, house):
 
 def check_user_permission(wizard):
     try:
-        HOGWARTS_ALUMNI_TABLE.get_item(
+        db_response = HOGWARTS_ALUMNI_TABLE.get_item(
             Key={
                 'username': wizard
             }
         )
+        item = db_response['Item']
         return True
     except Exception as e:
         print(e)
@@ -103,15 +107,25 @@ def get_house_leaderboard():
     house_points = {}
     for house in HOGWARTS_HOUSES:
         try:
-            db_response = HOGWARTS_ALUMNI_TABLE.scan(FilterExpression=boto3.dynamodb.conditions.Attr('house').eq(house))
+            db_response = HOGWARTS_ALUMNI_TABLE.scan(FilterExpression=boto3.dynamodb.conditions.Attr('house').eq(house.lower()))
+            print(db_response)
             items = db_response['Items']
-            house_points[house] = sum([i['points'] for i in items])
-            return house_points
+            house_points[house] = int(sum([i['points'] for i in items]))
         except Exception as e:
-            return "ERROR"
+            return e
+    return format_points(house_points)
+
+
+def format_points(house_points):
+    order_by_points = {house: points for house, points in sorted(house_points.items(), key=lambda x: x[1], reverse=True)}
+    report = ""
+    for prefix, (house, points) in zip(PREFIXES, order_by_points.items()):
+        report += f'_{prefix}*{house.capitalize()}* with *{points}* points_\n'
+    return report
 
 
 def lambda_handler(event, context):
+    print("EVENT: ", event)
     message = {}
     headers = event['headers']
     body = event['body']
@@ -126,7 +140,7 @@ def lambda_handler(event, context):
         assigner = params['user_name'][0]
 
         if len(text) == 1:
-            if text[0].upper() in HOGWARTS_HOUSES:
+            if text[0].lower() in HOGWARTS_HOUSES:
                 message = get_house_points(text[0].lower())
 
         if len(text) > 1:
@@ -134,13 +148,19 @@ def lambda_handler(event, context):
             if text[0:2] == ['set', 'house']:
                 # Text : /dumbledore set house @house
                 hogwarts_house = parse_potential_house(text[2])
-                ## RESPOND WITH ERROR IF POTENTIAL HOUSE NOT IN
-                message = create_wizard(assigner, hogwarts_house)
+                print("hogwarts_house ", hogwarts_house)
+                if hogwarts_house is not None:
+                    message = create_wizard(assigner, hogwarts_house)
+                else:
+                    message = {'text': f'Come on, Harry Potter started in 1997 you should know the house names by now: {HOGWARTS_HOUSES}'}
+                print("MESSAGE: ", message)
 
-            users, points = parse_slack_message(text)
+            #users, points = parse_slack_message(text)
     else:
         house_points = get_house_leaderboard()
         message = {'text': f'{house_points}'}
+
+    message["response_type"] = "in_channel"
 
     return {
         'statusCode': 200,
